7dd18b30e3050ee15a0c470a3cc12e02bbec0b1a
Andy Qin <qinyang_1980@qq.com>
2019-01-11 13:47:40 +0800

    更新nestfy版本

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,7 +4,7 @@ import { PhotoModule } from './photo/photo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    MongooseModule.forRoot('mongodb://localhost:27017/test', { useNewUrlParser: true }),
     PhotoModule
   ]
 })
